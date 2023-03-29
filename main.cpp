b07f605e6c947b152eb7649df206323e0264ffdf
YaroslavTihomolov <93517226+YaroslavTihomolov@users.noreply.github.com>
2023-03-29 23:46:34 +0700

    fix

--- main.cpp
+++ main.cpp
@@ -2,14 +2,12 @@
 #include "mpi.h"
 #include <iostream>
 #include <vector>
-#include <thread>
-#include <unistd.h>
 
 #define RANK_ROOT 0
 
 constexpr auto n1 = 9;
 constexpr auto n2 = 4;
-constexpr auto n3 = 2;
+constexpr auto n3 = 4;
 
 void fillMatrix(std::vector<int> &A, std::vector<int> &B) {
     A.resize(n1 * n2);
@@ -24,33 +22,42 @@ void fillMatrix(std::vector<int> &A, std::vector<int> &B) {
             B[j + i * n3] = j + i * n3 + 1;
         }
     }
-}
+ }
 
-void gathervRoutine(std::vector<int> &resultMatrix, std::vector<int> &recvcounts, std::vector<int> &displs, int size,
-                    int dims0, int dims1, int tmpMatrixColumn) {
-    resultMatrix.resize(n1 * n3);
-    recvcounts.resize(size);
-    std::fill(recvcounts.begin(), recvcounts.end(), 1);
-    displs.resize(size);
-    for (int i = 0; i < dims0; i++) {
-        for (int j = 0; j < dims1; j++) {
-            displs[j + i * dims1] = (j * tmpMatrixColumn + i * n3 * n1 / dims0);
-        }
-    }
-}
+ void gathervRoutine(std::vector<int> &resultMatrix, std::vector<int> &recvcounts, std::vector<int> &displs, int size,
+                     int dims0, int dims1, int tmpMatrixColumn) {
+     resultMatrix.resize(n1 * n3);
+     recvcounts.resize(size);
+     std::fill(recvcounts.begin(), recvcounts.end(), 1);
+     displs.resize(size);
+     for (int i = 0; i < dims0; i++) {
+         for (int j = 0; j < dims1; j++) {
+             displs[j + i * dims1] = (j * tmpMatrixColumn + i * n3 * n1 / dims0) / tmpMatrixColumn;
+         }
+     }
+ }
 
-void matrixMultiply(std::vector<int> &partA, std::vector<int> &partB, std::vector<int> &multiplyRes,
-                    int matrixALines, int matrixBColumns) {
-    for (int i = 0; i < matrixALines; i++) {
-        for (int j = 0; j < matrixBColumns; j++) {
-            int sum = 0;
-            for (int k = 0; k < n2; k++) {
-                sum += partA[i * n2 + k] * partB[k * matrixBColumns + j];
-            }
-            multiplyRes[i * matrixBColumns + j] = sum;
-        }
-    }
-}
+ void matrixMultiply(std::vector<int> &partA, std::vector<int> &partB, std::vector<int> &multiplyRes,
+                     int matrixALines, int matrixBColumns) {
+     for (int i = 0; i < matrixALines; i++) {
+         for (int j = 0; j < matrixBColumns; j++) {
+             int sum = 0;
+             for (int k = 0; k < n2; k++) {
+                 sum += partA[i * n2 + k] * partB[k * matrixBColumns + j];
+             }
+             multiplyRes[i * matrixBColumns + j] = sum;
+         }
+     }
+ }
+
+ void printMatrix(std::vector<int> &matrix, int columns, int lines) {
+     for (int i = 0; i < columns; i++) {
+         for (int j = 0; j < lines; j++) {
+             std::cout << matrix[i * n3 + j] << " ";
+         }
+         std::cout << '\n';
+     }
+ }
 
 void Run() {
     int dims[2] = {0, 0}, periods[2] = {0, 0}, coords[2], reorder = 0;
@@ -65,6 +72,7 @@ void Run() {
     MPI_Cart_get(comm2d, 2, dims, periods, coords);
 
     int tmpMatrixColumn = n3 / dims[1];
+    int tmpMatrixLines = n1 / dims[0];
 
 
     MPI_Datatype row_type, col_type, col_type_resized;
@@ -95,7 +103,7 @@ void Run() {
 
 
     if (coords[1] == 0) {
-        MPI_Scatter(A.data(), n1 / dims[0], row_type, partA.data(), n1 / dims[0], row_type, RANK_ROOT, commY);
+        MPI_Scatter(A.data(), tmpMatrixLines, row_type, partA.data(), tmpMatrixLines, row_type, RANK_ROOT, commY);
     }
     if (coords[0] == 0) {
         MPI_Scatter(B.data(), 1, col_type_resized, partB.data(), tmpMatrixColumn * n2, MPI_INT, RANK_ROOT, commX);
@@ -105,12 +113,12 @@ void Run() {
     MPI_Bcast(partA.data(), p1, MPI_INT, RANK_ROOT, commX);
     MPI_Bcast(partB.data(), p2, MPI_INT, RANK_ROOT, commY);
 
-    std::vector<int> multiplyRes(n1 / dims[0] * tmpMatrixColumn);
+    std::vector<int> multiplyRes(tmpMatrixLines * tmpMatrixColumn);
 
-    matrixMultiply(partA, partB, multiplyRes, n1 / dims[0], tmpMatrixColumn);
+    matrixMultiply(partA, partB, multiplyRes, tmpMatrixLines, tmpMatrixColumn);
 
     MPI_Datatype matrix_back, matrix_back_resized;
-    MPI_Type_vector(n1 / dims[0], (int) tmpMatrixColumn, n3, MPI_INT, &matrix_back);
+    MPI_Type_vector(tmpMatrixLines, (int) tmpMatrixColumn, n3, MPI_INT, &matrix_back);
     MPI_Type_commit(&matrix_back);
     MPI_Type_create_resized(matrix_back, 0, (int) tmpMatrixColumn * sizeof(int), &matrix_back_resized);
     MPI_Type_commit(&matrix_back_resized);
@@ -120,35 +128,16 @@ void Run() {
     std::vector<int> displs;
     if (rank == RANK_ROOT) {
         gathervRoutine(resultMatrix, recvcounts, displs, size, dims[0], dims[1], tmpMatrixColumn);
-        for (auto i : displs) {
-            std::cout << i << ' ';
-        }
-        std::cout << '\n';
     }
-
-/*    sleep(rank);
-    for (auto i : multiplyRes) {
-        std::cout << i << ' ';
-    }
-    std::cout << '\n';*/
-
 
     MPI_Gatherv(multiplyRes.data(), multiplyRes.size(), MPI_INT, resultMatrix.data(), recvcounts.data(), displs.data(),
                 matrix_back_resized, RANK_ROOT, MPI_COMM_WORLD);
+
     if (rank == RANK_ROOT) {
-        for (int i = 0; i < n1; i++) {
-            for (int j = 0; j < n3; j++) {
-                std::cout << resultMatrix[i * n3 + j] << " ";
-            }
-            std::cout << '\n';
-        }
+        printMatrix(resultMatrix, n1, n3);
     }
 
-    MPI_Type_free(&matrix_back);
-    MPI_Type_free(&matrix_back_resized);
-    MPI_Type_free(&row_type);
-    MPI_Type_free(&col_type);
-    MPI_Type_free(&col_type_resized);
+
 }
 
 
@@ -159,164 +148,3 @@ int main(int argc, char **argv) {
     MPI_Finalize();
     return 0;
 }
-
-
-/*#include <cmath>
-#include "mpi.h"
-#include <iostream>
-#include <vector>
-#include <thread>
-#include <unistd.h>
-
-#define RANK_ROOT 0
-
-constexpr auto n1 = 4;
-constexpr auto n2 = 4;
-constexpr auto n3 = 4;
-
-void fillMatrix(std::vector<int> &A, std::vector<int> &B) {
-    A.resize(n1 * n2);
-    for (int i = 0; i < n1; i++) {
-        for (int j = 0; j < n2; j++) {
-            if (i == j) {
-                A[j + i * n2] = 2;
-            } else {
-                A[j + i * n2] = 1;
-            }
-        }
-    }
-    B.resize(n2 * n3);
-    for (int i = 0; i < n2; i++) {
-        for (int j = 0; j < n3; j++) {
-            if (i == j) {
-                B[j + i * n3] = 2;
-            } else {
-                B[j + i * n2] = 1;
-            }
-        }
-    }
-}
-
-void matrixMultiply(std::vector<int> &partA, std::vector<int> &partB, std::vector<int> &multiplyRes,
-                    int matrixALines, int matrixBColumns) {
-    for (int i = 0; i < matrixALines; i++) {
-        for (int j = 0; j < matrixBColumns; j++) {
-            int sum = 0;
-            for (int k = 0; k < n2; k++) {
-                sum += partA[i * n2 + k] * partB[k * matrixBColumns + j];
-            }
-            multiplyRes[i * matrixBColumns + j] = sum;
-        }
-    }
-}
-
-void Run() {
-    int dims[2] = {0, 0}, periods[2] = {0, 0}, coords[2], reorder = 0;
-    int size, rank;
-    MPI_Comm comm2d;
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-    MPI_Dims_create(size, 2, dims);
-
-    MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, reorder, &comm2d);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Cart_get(comm2d, 2, dims, periods, coords);
-
-    MPI_Datatype row_type, col_type, col_type_resized;
-    MPI_Type_contiguous(n2, MPI_INT, &row_type);
-    MPI_Type_vector(n2, (int) tmpMatrixColumn, n3, MPI_INT, &col_type);
-    MPI_Type_commit(&row_type);
-    MPI_Type_commit(&col_type);
-    MPI_Type_create_resized(col_type, 0, (int) tmpMatrixColumn * sizeof(int), &col_type_resized);
-    MPI_Type_commit(&col_type_resized);
-
-    MPI_Comm commX, commY;
-
-    MPI_Comm_split(comm2d, coords[0], rank, &commX);
-    MPI_Comm_split(comm2d, coords[1], rank, &commY);
-
-    const auto p1 = n1 * n2 / dims[0];
-    const auto p2 = n2 * tmpMatrixColumn;
-
-    std::vector<int> partA(p1);
-    std::vector<int> partB(p2);
-
-
-    std::vector<int> A;
-    std::vector<int> B;
-    if (rank == RANK_ROOT) {
-        fillMatrix(A, B);
-    }
-
-
-    if (coords[1] == 0) {
-        MPI_Scatter(A.data(), n1 / dims[0], row_type, partA.data(), n1 / dims[0], row_type, RANK_ROOT, commY);
-    }
-    if (coords[0] == 0) {
-        MPI_Scatter(B.data(), 1, col_type_resized, partB.data(), tmpMatrixColumn * n2, MPI_INT, RANK_ROOT, commX);
-    }
-
-
-    MPI_Bcast(partA.data(), p1, MPI_INT, RANK_ROOT, commX);
-    MPI_Bcast(partB.data(), p2, MPI_INT, RANK_ROOT, commY);
-
-    std::vector<int> multiplyRes(n1 / dims[0] * tmpMatrixColumn);
-
-    matrixMultiply(A, B, multiplyRes, n1 / dims[0], tmpMatrixColumn);
-
-    sleep(rank);
-    for (int i = 0; i < dims[0]; i++) {
-        for (int j = 0; j < dims[1]; j++) {
-            std::cout << multiplyRes[i * dims[1] + j] << " ";
-        }
-        std::cout << '\n';
-    }
-
-MPI_Datatype matrix_back, matrix_back_resized;
-MPI_Type_vector(n1 / dims[0], (int) tmpMatrixColumn, n3, MPI_INT, &matrix_back);
-MPI_Type_commit(&matrix_back);
-MPI_Type_create_resized(matrix_back, 0, (int) tmpMatrixColumn * sizeof(int), &matrix_back_resized);
-MPI_Type_commit(&matrix_back_resized);
-
-std::vector<int> resultMatrix;
-std::vector<int> recvcounts;
-std::vector<int> displs;
-if (rank == RANK_ROOT) {
-resultMatrix.resize(n1 * n3);
-recvcounts.resize(size);
-std::fill(recvcounts.begin(), recvcounts.end(), 1);
-displs.resize(size);
-for (int i = 0; i < dims[0]; i++) {
-for (int j = 0; j < dims[1]; j++) {
-displs[j + i * dims[1]] = (j * tmpMatrixColumn + i * tmpMatrixColumn * n1) / 2;
-}
-}
-}
-
-
-MPI_Gatherv(multiplyRes.data(), 4, MPI_INT, resultMatrix.data(), recvcounts.data(), displs.data(), matrix_back_resized, RANK_ROOT, MPI_COMM_WORLD);
-if (rank == RANK_ROOT) {
-for (int i = 0; i < n1; i++) {
-for (int j = 0; j < n3; j++) {
-std::cout << resultMatrix[i * n3 + j] << " ";
-}
-std::cout << '\n';
-}
-}
-
-MPI_Type_free(&matrix_back);
-MPI_Type_free(&matrix_back_resized);
-MPI_Type_free(&row_type);
-MPI_Type_free(&col_type);
-MPI_Type_free(&col_type_resized);
-}
-
-
-int main(int argc, char **argv) {
-
-    MPI_Init(&argc, &argv);
-    Run();
-    MPI_Finalize();
-    return 0;
-}
-*/
